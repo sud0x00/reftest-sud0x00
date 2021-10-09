e4595fc6191a1763eec647cc8f9c09b082147372
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2021-10-09 16:52:25 +0530

    Update problem3.c

--- reftest/problem3.c
+++ reftest/problem3.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
+struct aot
+{
+  float h,b,a;
+};
+struct aot x;
+float height()
+{
+  printf("Length of height");
+  scanf("%f",&x.h);
+  
+}
+float base()
+{
+  printf("Length of base");
+  scanf("%f", &x.b);
+  
+}
+float area()
+{
+  x.a= 0.5*x.h*x.b;
+  
+}
+void op()
+{
+    printf("\n Area of triangle  = %.5f",x.a);
+}
+
+int main()
+{
+  height();
+  base();
+  area();
+  op();
+  return 0;
+}
+
+
+
 
