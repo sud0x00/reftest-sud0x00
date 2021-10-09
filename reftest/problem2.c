165225d79e37cce15472ba6d0e581ff2c4e0b834
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2021-10-09 15:59:27 +0530

    Update problem2.c

--- reftest/problem2.c
+++ reftest/problem2.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+float base()
+{
+  float q;
+  printf("\n Enter the size of the base");
+  scanf("%f",&q);
+  return q;
+}
+float height()
+{
+  float w;
+  printf("\n Enter the size of the height");
+  scanf("%f",&w);
+  return w;
+}
+float area(float hx, float bx)
+{
+  float Area;
+  Area = 0.5*hx*bx;
+  return Area;
+}
+float print(float ar)
+{
+   printf("\n Area of triangle is = %.5f",ar);
+  return 0;
+}
+float main()
+{
+  float h1 = height();
+  float b1 = base();
+  float a = area(h1,b1);
+  print(a);
+  return 0;
+}
 
