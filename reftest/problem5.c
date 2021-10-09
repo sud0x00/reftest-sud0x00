197d3ee08f8cab532ae93d8a899ecb501da4523f
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2021-10-09 19:01:13 +0530

    Update problem5.c

--- reftest/problem5.c
+++ reftest/problem5.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+#include<math.h>
+float i_p()
+{
+    float x;
+    printf("Enter the value of X");
+    scanf("%f",&x);
+    return x;
+}
+float calc(float a)
+{
+    float res = exp(a);
+    return res;
+}
+void o_p(float r , float x)
+{
+    printf("Exponential value of %f is %.3f",x,r);
+}
+int main()
+{
+    float x_value,result;
+    x_value = i_p();
+    result = calc(x_value);
+    o_p(result , x_value);
+}
