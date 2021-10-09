2026b6dd7f22cc4f53cd3d686c923ce2e7440026
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2021-10-09 15:44:59 +0530

    Update problem1.c

--- reftest/problem1.c
+++ reftest/problem1.c
@@ -1,1 +1,9 @@
-
+#include<stdio.h>
+void main()
+{
+  float base , height , Area;
+  printf("Enter length of base and height");
+  scanf("%f%f",&base,&height);
+  Area = 0.5*base*height;
+  printf("Area of the given triangle is %.5f",Area);
+}
