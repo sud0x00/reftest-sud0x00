98dd3c6af6e09bcead3a0ee7ca6ce87042d98920
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2021-10-14 18:27:48 +0530

    Update problem8.c

--- reftest/problem8.c
+++ reftest/problem8.c
@@ -1,1 +1,58 @@
+#include<stdio.h>
+struct triangle
+{
+	float height, base, area;
+}t[100];
+int num()
+{
+	int n;
+	printf("Enter number of triangles : ");
+	scanf("%d",&n);
+	
+	return n;
+}
+float h(int n)
+{
+	float height;
+	printf("Enter the height of %d triangle: ", n);
+	scanf("%f",&height);
+	printf("\n");
+	return height;
 
+}
+float b(int n)
+{
+    float base;
+	printf("Enter the base of %d triangle: ", n);
+	scanf("%f",&base);
+	printf("\n");
+	return base;
+}
+float area(float height, float base)
+{
+	float area;
+	area = 0.5*height*base;
+	return area;
+}
+int main()
+{
+	struct triangle t[100];
+	int n;
+	n = num();
+	
+	for(int i=0;i<n;i++)
+	{
+		t[i].height = h(i+1);
+		t[i].base = b(i+1);
+		printf("\n");
+	}
+	for( int i=0;i<n;i++)
+	{
+		t[i].area = area(t[i].height,t[i].base);
+	}
+	for(int i=0;i<n;i++)
+	{
+		printf("Area of %d triangle is %f " ,i+1 , t[i].area);
+	}
+	return 0;
+}
